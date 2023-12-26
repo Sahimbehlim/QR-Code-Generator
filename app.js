6490c72cac951db2323bee061ee8af7864984f4a
Sahim Behlim <126802461+Sahimbehlim@users.noreply.github.com>
2023-12-26 10:25:50 +0530

    added css and js file

---
+++ app.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector('.wrapper');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const qrImg = document.querySelector('img');
+
+btn.addEventListener('click',() => {
+    let qrValue = input.value;
+    input.placeholder = qrValue ? "Enter text or url" : "Input can't be empty";
+    if(!qrValue) return;
+    btn.innerText = 'Generating...';
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+    qrImg.addEventListener('load',() => {
+        wrapper.classList.add('active');
+        btn.innerText = 'Generate QR Code';
+    });
+});
